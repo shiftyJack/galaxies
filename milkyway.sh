1f2bbf162ae0a8f76d38eeb9f338c28f9da0c87e
Matt Grant <matthew.jordan.grant@gmail.com>
2024-04-01 19:34:32 -0230

    added redshift values

---
+++ milkyway.sh
@@ -0,0 +1,7 @@
+Galaxy name = Milky Way
+
+The Milky Way is estimated to contain 100-400 billion stars.
+
+At the center of the Milky Way is a supermassive black
+hole called "Sagittarius A*", with a mass of approximately
+4000 solar masses.
